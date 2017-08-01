6c2ff8ae77ea7b9ca344e0684497a6762875c31e
Zack Thoutt <zackarey.thoutt@colorado.edu>
2017-07-31 21:29:42 -0600

    feat: create base molecule component

---
+++ static/apps/promotion/src/app/chemical-solution/molecule/molecule.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-molecule',
+  templateUrl: './molecule.html',
+  styleUrls: ['./molecule.scss']
+})
+export class MoleculeComponent { }
