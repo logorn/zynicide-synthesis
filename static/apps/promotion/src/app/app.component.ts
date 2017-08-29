a9879b6c656e11f5d8234f72a428768281645cda
Zack Thoutt <zackarey.thoutt@colorado.edu>
2017-08-29 17:08:00 -0600

    feat: add typed section for personal titles

--- static/apps/promotion/src/app/app.component.ts
+++ static/apps/promotion/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import * as Typed from 'typed.js';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    title = 'app';
+
+    constructor(private el: ElementRef) { }
+
+    ngOnInit() {
+		let typed = new Typed(this.el.nativeElement.querySelector("#what-i-am"), {
+		    strings: [
+		    	"Web Developer",
+		    	"Data Scientist",
+		    	"Amature Designer",
+		    	"World Traveler",
+		    	"Avid Hiker",
+		    	"Landscape Photographer"
+		    ],
+		    typeSpeed: 60,
+		    backSpeed: 60,
+		    backDelay: 1500,
+		    loop: true
+		});
+    }
 }
